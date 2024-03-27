56f6a63558600678511367a65efe6d229bcc0efa
Gourav soni <gouravsoni8476@gmail.com>
2024-03-27 21:10:30 +0530

    server dir

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import emojiAPI from "./emojiApi.json" assert { type: "json" };
+import cors from "cors";
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get("/search", (req, res) => {
+  console.log(req.query);
+  let result = [];
+  (function () {
+    const { q = "win", count = 10 } = req.query;
+
+    if (Number(count) > 20) {
+      return res.send('"count" should be less than 20');
+    } else {
+      let emojiAPIFilteredResult = emojiAPI.filter((item) => {
+        return (
+          item.keywords.toLowerCase().includes(q.toLowerCase()) ||
+          item.title.toLowerCase().includes(q.toLowerCase())
+        );
+      });
+      result.push(...emojiAPIFilteredResult.slice(0, Number(count)));
+    }
+  })();
+
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
